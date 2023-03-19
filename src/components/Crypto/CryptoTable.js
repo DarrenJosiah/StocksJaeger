0a29d5d341b0690ebb7a8f463cfbcfa2d1a3c5f8
Azrio <38596941+Zechst@users.noreply.github.com>
2023-03-19 14:16:31 +0800

    fix: useEffect dependancy, chore: change class to className

--- src/components/Crypto/CryptoTable.js
+++ src/components/Crypto/CryptoTable.js
@@ -1,112 +1,124 @@
-import React from 'react'
-import dummyJson from '../dummy.json'
+import React from "react";
+import dummyJson from "../dummy.json";
 
 function CryptoTable(props) {
-
-    // https://www.livecoinwatch.com/
-    // https://livecoinwatch.github.io/lcw-api-docs/#coinslist
+  // https://www.livecoinwatch.com/
+  // https://livecoinwatch.github.io/lcw-api-docs/#coinslist
 
   return (
-    <div class='m-8'>
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3 bg-gray-100 dark:bg-gray-800">
-                            Rank 
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-50">
-                            Coin
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-100 dark:bg-gray-800">
-                            Price
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-50">
-                            Market Cap
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-100 dark:bg-gray-800">
-                            24h Volume
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-50">
-                            All-Time High (USD)
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-100 dark:bg-gray-800">
-                            1H
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-50">
-                            1D
-                        </th>
-                    </tr>
-                </thead>
-                <tbody> 
-                        {dummyJson.map(crypto => {
-                            return (
-                            <tr class="border-b border-gray-200 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
-                                    <span>{crypto.rank}</span>
-                                </th>
-                                <td class="px-6 py-4 flex flex-row items-center bg-gray-50">
-                                    <img class='mr-4' src={crypto.png32}></img>
-                                    <div class='flex flex-col items-start'>
-
-                                        <span>{crypto.name}</span>
-                                        <small>{crypto.code}</small>
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4 bg-gray-100 dark:bg-gray-800">
-                                    ${crypto.rate.toFixed(2)}
-                                </td>
-                                {
-                                    (crypto.cap/10000000000).toFixed(2) >= 1 ?
-                                    <td class="px-6 py-4 bg-gray-50">
-                                        ${(crypto.cap/10000000000).toFixed(2)}B
-                                    </td>
-                                    :
-                                    <td class="px-6 py-4 bg-gray-50">
-                                        ${(crypto.cap/10000000).toFixed(2)}M
-                                    </td>
-                                }
-                                {
-                                    (crypto.volume/10000000000).toFixed(2) >= 1 ?
-                                    <td class="px-6 py-4 bg-gray-100">
-                                        ${(crypto.volume/10000000000).toFixed(2)}B
-                                    </td>
-                                    :
-                                    <td class="px-6 py-4 bg-gray-100">
-                                        ${(crypto.volume/10000000).toFixed(2)}M
-                                    </td>
-                                }
-                                <td class="px-6 py-4 bg-gray-50">
-                                    ${crypto.allTimeHighUSD.toFixed(2)}
-                                </td>
-                                { 
-                                    crypto.delta.hour.toFixed(2) >= 1 ?
-                                    <td class="px-6 bg-green-100 dark:bg-gray-800">
-                                        {crypto.delta.hour.toFixed(2)}%
-                                    </td>
-                                    :
-                                    <td class="px-6 bg-red-100 dark:bg-gray-800">
-                                        {crypto.delta.hour.toFixed(2)}%
-                                    </td>
-                                }
-                                { 
-                                    crypto.delta.day.toFixed(2) >= 1 ?
-                                    <td class="px-6 bg-green-100 dark:bg-gray-800">
-                                        {crypto.delta.day.toFixed(2)}%
-                                    </td>
-                                    :
-                                    <td class="px-6 bg-red-100 dark:bg-gray-800">
-                                        {crypto.delta.day.toFixed(2)}%
-                                    </td>
-                                }
-                            </tr>
-                            )
-                        })}
-                </tbody>
-            </table>
-        </div>
+    <div className="m-8">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-3 bg-gray-100 dark:bg-gray-800"
+              >
+                Rank
+              </th>
+              <th scope="col" className="px-6 py-3 bg-gray-50">
+                Coin
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 bg-gray-100 dark:bg-gray-800"
+              >
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3 bg-gray-50">
+                Market Cap
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 bg-gray-100 dark:bg-gray-800"
+              >
+                24h Volume
+              </th>
+              <th scope="col" className="px-6 py-3 bg-gray-50">
+                All-Time High (USD)
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 bg-gray-100 dark:bg-gray-800"
+              >
+                1H
+              </th>
+              <th scope="col" className="px-6 py-3 bg-gray-50">
+                1D
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {dummyJson.map((crypto) => {
+              return (
+                <tr
+                  key={crypto.name}
+                  className="border-b border-gray-200 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800"
+                  >
+                    <span>{crypto.rank}</span>
+                  </th>
+                  <td className="px-6 py-4 flex flex-row items-center bg-gray-50">
+                    <img className="mr-4" src={crypto.png32}></img>
+                    <div className="flex flex-col items-start">
+                      <span>{crypto.name}</span>
+                      <small>{crypto.code}</small>
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 bg-gray-100 dark:bg-gray-800">
+                    ${crypto.rate.toFixed(2)}
+                  </td>
+                  {(crypto.cap / 10000000000).toFixed(2) >= 1 ? (
+                    <td className="px-6 py-4 bg-gray-50">
+                      ${(crypto.cap / 10000000000).toFixed(2)}B
+                    </td>
+                  ) : (
+                    <td className="px-6 py-4 bg-gray-50">
+                      ${(crypto.cap / 10000000).toFixed(2)}M
+                    </td>
+                  )}
+                  {(crypto.volume / 10000000000).toFixed(2) >= 1 ? (
+                    <td className="px-6 py-4 bg-gray-100">
+                      ${(crypto.volume / 10000000000).toFixed(2)}B
+                    </td>
+                  ) : (
+                    <td className="px-6 py-4 bg-gray-100">
+                      ${(crypto.volume / 10000000).toFixed(2)}M
+                    </td>
+                  )}
+                  <td className="px-6 py-4 bg-gray-50">
+                    ${crypto.allTimeHighUSD.toFixed(2)}
+                  </td>
+                  {crypto.delta.hour.toFixed(2) >= 1 ? (
+                    <td className="px-6 bg-green-100 dark:bg-gray-800">
+                      {crypto.delta.hour.toFixed(2)}%
+                    </td>
+                  ) : (
+                    <td className="px-6 bg-red-100 dark:bg-gray-800">
+                      {crypto.delta.hour.toFixed(2)}%
+                    </td>
+                  )}
+                  {crypto.delta.day.toFixed(2) >= 1 ? (
+                    <td className="px-6 bg-green-100 dark:bg-gray-800">
+                      {crypto.delta.day.toFixed(2)}%
+                    </td>
+                  ) : (
+                    <td className="px-6 bg-red-100 dark:bg-gray-800">
+                      {crypto.delta.day.toFixed(2)}%
+                    </td>
+                  )}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CryptoTable
+export default CryptoTable;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,27 @@
-import News from './News';
-import Particle from './Particle';
+import News from "./News";
+import Particle from "./Particle";
 
-function Home() {
+export default function Home() {
   return (
     <div>
-      <p class="text-3xl font-bold mt-10">Welcome to</p>
-      <p class="text-7xl font-bold text-blue-900 mb-3">Stocks Jaeger</p>
-      <p class="text-1xl font-bold mb-8">Home for the ETF, Crypto and Forex hunters</p>
-    
-      <img class="place-items-center p-3 display-flex rounded-3xl" src='MyImages/neon.jpeg'></img>
-      {/* <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#PhotoBy</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#RichSmith</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Unsplash</span> */}
+      <p className="text-3xl font-bold mt-10">Welcome to</p>
+      <p className="text-7xl font-bold text-blue-900 mb-3">Stocks Jaeger</p>
+      <p className="text-1xl font-bold mb-8">
+        Home for the ETF, Crypto and Forex hunters
+      </p>
 
-      <div className='home' class='grid mt-8 place-items-center'>
+      <img
+        className="place-items-center p-3 display-flex rounded-3xl"
+        src="MyImages/neon.jpeg"
+      ></img>
+      {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#PhotoBy</span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#RichSmith</span>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Unsplash</span> */}
+
+      <div className="grid mt-8 place-items-center">
         <News />
         <Particle />
       </div>
-      </div>
-  )
+    </div>
+  );
 }
-
-export default Home
